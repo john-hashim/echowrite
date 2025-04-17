39b52eec3df042f0118fb9221e0fc21ebd4fb1f0
John Hashim <johnhashim@Johns-MacBook-Air.local>
2025-04-17 16:37:36 +0530

    shadcn ui added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,10 @@
-import './App.css'
+import { Button } from "@/components/ui/button"
 
 function App() {
 
   return (
     <> 
-      <h1 className='text-red-500'>hello</h1>
+      <Button>Button</Button>
     </>
   )
 }
