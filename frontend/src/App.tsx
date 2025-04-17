96dcb318f61240b703050c5c76e3e7eee0a741ff
John Hashim <johnhashim@Johns-MacBook-Air.local>
2025-04-17 17:18:35 +0530

    theme added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,14 @@
 import { Button } from "@/components/ui/button"
+import { ThemeProvider } from "@/contexts/theme-provider"
+import { ThemeToggle } from "@/components/common/theme-toggle"
 
 function App() {
 
   return (
-    <> 
+    <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
       <Button>Button</Button>
-    </>
+      <ThemeToggle />
+    </ThemeProvider>
   )
 }
 
